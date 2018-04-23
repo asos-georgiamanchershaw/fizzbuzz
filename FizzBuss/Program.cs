199addceb68820434f1a824973a164042d55280a
Georgia Manchershaw <georgia.manchershaw@asos.com>
2018-04-23 11:07:39 +0100

    Add project files.

---
+++ FizzBuss/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using FizzBuss.Validators;
+
+namespace FizzBuzz
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                bool isDivisibleByThree = InputValidator.IsDivisibleBy(i, 3);
+                bool isDivisibleByFive = InputValidator.IsDivisibleBy(i, 5);
+                bool isDivisibleByThreeAndFive = InputValidator.IsDivisibleBy(i, 3) && InputValidator.IsDivisibleBy(i, 5);
+                bool containsThree = InputValidator.ContainsNumber(i, 3);
+                bool containsFive = InputValidator.ContainsNumber(i, 5);
+
+                if (isDivisibleByThreeAndFive)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (isDivisibleByThree || containsThree)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (isDivisibleByFive || containsFive)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FizzBuss/Validators/InputValidator.cs
@@ -0,0 +1,28 @@
+﻿namespace FizzBuss.Validators
+{
+    public class InputValidator
+    {
+        public static bool IsDivisibleBy(int input, int divisibleBy)
+        {
+            if (input % divisibleBy == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ContainsNumber(int input, int number)
+        {
+            // what happens for 35... contains 3, 5 and divisible by 5
+            
+            if (input.ToString().Contains(number.ToString()))
+            {
+                return true;
+            }
+
+            return false;
+
+        }
+    }
+}
